a2b5a911f54fbc2338ccfa2af3c66cdf37d77702
HSmart <howard1.smart1@gmail.com>
2020-10-16 07:23:11 -0400

    equations update
    updated line 31 and  32 shortening the equations.

--- Sum.cs
+++ Sum.cs
@@ -28,8 +28,8 @@ namespace Sums
 
             while (n > 0)
             {
-                sum = sum + (n % 10);
-                n = n / 10;
+                sum += (n % 10);
+                n /= 10;
             }
 
             Console.Write("Sum is= " + sum);
